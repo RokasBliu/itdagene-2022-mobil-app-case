8ba246edc6ea2021545931ae406a2a834000b29b
Joachim Andreassen <joachima94@gmail.com>
2022-09-21 12:30:46 +0200

    Upfixed highscore

--- flutter/lib/pages/highscore.dart
+++ flutter/lib/pages/highscore.dart
@@ -14,27 +14,37 @@ class Highscore extends StatelessWidget {
   List<Center> highscoreWidgets(HighscoreEntryCollection collection) {
     List<Center> widgets = List.empty(growable: true);
     var value_list = collection.entries.values.toList();
+    var style =
+        const TextStyle(fontSize: 30, color: Color.fromRGBO(255, 0, 0, 1.0));
+    if (value_list.isEmpty) {
+      return [
+        Center(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50.0),
+                child: RichText(
+                    text: TextSpan(text: "-------------", style: style))))
+      ];
+    }
     value_list.sort((a, b) => b.score.compareTo(a.score));
     var iter = value_list.iterator;
     var pos = 0;
-    while(iter.moveNext()) {
+    while (iter.moveNext()) {
       pos += 1;
       var cur = iter.current;
       var name = cur.name;
       var score = cur.score;
-      var style = const TextStyle(fontSize: 30, color: Color.fromRGBO(255, 0, 0, 1.0));
-      widgets.add(Center(child: Padding(padding: EdgeInsets.symmetric(horizontal: 50.0),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-        RichText(
-            text: TextSpan(text: "$pos.", style: style)
-        ),
-        RichText(
-            text: TextSpan(text: name, style: style)
-        ),
-        RichText(
-            text: TextSpan(text: "$score", style: style)
-        )
-      ]))));
+      var style =
+          const TextStyle(fontSize: 30, color: Color.fromRGBO(255, 0, 0, 1.0));
+      widgets.add(Center(
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(text: TextSpan(text: "$pos.", style: style)),
+                    RichText(text: TextSpan(text: name, style: style)),
+                    RichText(text: TextSpan(text: "$score", style: style))
+                  ]))));
     }
     return widgets;
   }
@@ -43,16 +53,31 @@ class Highscore extends StatelessWidget {
   Widget build(BuildContext context) {
     return FutureBuilder(
         future: _loadHighscore(),
-        builder: (BuildContext context, AsyncSnapshot<HighscoreEntryCollection> snapshot) {
+        builder: (BuildContext context,
+            AsyncSnapshot<HighscoreEntryCollection> snapshot) {
           HighscoreEntryCollection? collection = snapshot.data;
           if (collection != null) {
-            var title = Container(padding: const EdgeInsets.symmetric(vertical: 25.0),child: RichText(text: const TextSpan(text:"HIGHSCORE", style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold, color: Color.fromRGBO(255, 0, 0, 1.0)))));
+            var title = Container(
+                padding: const EdgeInsets.symmetric(vertical: 25.0),
+                child: Padding(padding: EdgeInsets.only(top: 50.0),
+                child: RichText(
+                    text: const TextSpan(
+                        text: "HIGHSCORE",
+                        style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(255, 0, 0, 1.0))))));
             var highscoreWidgetList = highscoreWidgets(collection);
-            return Column(children: [Center(child: title), Column(crossAxisAlignment: CrossAxisAlignment.start,children: highscoreWidgetList,)]);
+            return Column(children: [
+              Center(child: title),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: highscoreWidgetList,
+              )
+            ]);
           } else {
             return const Text("Loading");
           }
-        }
-    );
+        });
   }
 }
