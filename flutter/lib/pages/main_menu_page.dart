424c19835c3e6be6a25087c372177fad8e74528e
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-09-21 14:45:35 +0200

    We try again

--- flutter/lib/pages/main_menu_page.dart
+++ flutter/lib/pages/main_menu_page.dart
@@ -11,34 +11,46 @@ class MainMenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(255, 240, 235, 1.0),
+      backgroundColor: Color.fromRGBO(255, 240, 235, 1.0),
       body: SafeArea(
         // child: Game(key: key),
         child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
           Expanded(
-            child: Column(
-              children: const [
-                Center(
-                    child: Image(image: AssetImage('images/logo_black.png'))),
-                Center(
-                  child: Text(
-                    'KNOWELL',
-                    style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: Column(
+                children: const [
+                  Center(
+                      child: Image(image: AssetImage('images/logo_black.png'))),
+                  Center(
+                    child: Text(
+                      'KNOWELL',
+                      style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(0.0, 60.0, 0.0, 0.0),
             child: Column(children: [
-              _modeButton(context, Scaffold(backgroundColor: Color.fromRGBO(255, 240, 235, 1.0), body: Game(key: key)), "GAME"),
               _modeButton(
-                  context, Scaffold(backgroundColor: Color.fromRGBO(255, 240, 235, 1.0), body: Highscore(key: key)), "HIGHSCORE"),
+                  context,
+                  Scaffold(
+                      backgroundColor: Color.fromRGBO(255, 240, 235, 1.0),
+                      body: Game(key: key)),
+                  "GAME"),
+              _modeButton(
+                  context,
+                  Scaffold(
+                      backgroundColor: Color.fromRGBO(255, 240, 235, 1.0),
+                      body: Highscore(key: key)),
+                  "HIGHSCORE"),
             ]),
           ),
         ]),
